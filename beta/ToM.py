f867c7ba6322f80915eacee014d275c4422dcb9c
daddabarba <davidebarbieri97@gmail.com>
2019-10-21 22:14:42 +0200

    Parallel U computation

--- beta/ToM.py
+++ beta/ToM.py
@@ -23,13 +23,17 @@ class Beliefs0:
 		self.b[context, delta] += 1
 
 
-def V(OSeller, OBuyer):
+def V(OSeller, OBuyer, i=-1):
 
-	n = max(np.size(OSeller), np.size(OBuyer))
-	i = -1 if np.size(OSeller)>1 else 1
+	v = i*OBuyer*np.ones([np.size(OSeller), np.size(OBuyer)])
+	v[OSeller.T > OBuyer] = PENALTY
 
-	v = i*OBuyer*np.ones(n)
-	v[OSeller > OBuyer] = PENALTY
+	if i<0:
+		return v.T
 
 	return v
+
+
+def U(v, p):
+	return v.dot(p)
 
