a4436dd0c10a82b3e532267fbba7044d578ab41f
daddabarba <davidebarbieri97@gmail.com>
2019-10-21 21:30:21 +0200

    added first draft of beliefs class

---
+++ beta/ToM.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class Beliefs0:
+
+	def __init__(self, n_deltas, n_contexts):
+
+		self.b = np.ones([n_contexts, n_deltas])
+
+	def __call__(self, context, delta=None):
+
+		p = self.b[context]
+		s = np.sum(p)
+
+		if not delta:
+			return p/s
+
+		return p[delta]/s
+
+
+	def observe(self, context, delta):
+		self.b[context, delta] += 1
+
