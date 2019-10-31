2a39ed074ab179990f6ef65840cb020844447663
Kaustav <kaustv.datta@gmail.com>
2019-10-31 15:29:17 +0100

    visualization color code update

--- beta/visualization.py
+++ beta/visualization.py
@@ -15,18 +15,30 @@ n_agents = int(CONFIG_MODEL['total_agents'])
 grid_height = int(CONFIG_MODEL['grid_height'])
 grid_width = int(CONFIG_MODEL['grid_width'])
 
+
 def agent_portrayal(agent):
     portrayal = {"Shape": "circle",
                  "Filled": "true",
                  "Layer": 0,
-                 "Color": "red",
+                 #  "Color": "red",
                  "r": 0.5}
+
+    if agent.strategy == 'hawk':
+        portrayal["Color"] = "red"
+    elif agent.strategy == 'dove':
+        portrayal["Color"] = "yellow"
+    elif agent.strategy == 'possessor':
+        portrayal["Color"] = "blue"
+    elif agent.strategy == 'trader':
+        portrayal["Color"] = "green"
+
     return portrayal
+
 
 grid = CanvasGrid(agent_portrayal, grid_width, grid_height, 500, 500)
 server = ModularServer(EvolutionaryModel,
                        [grid],
                        "Evolutionary Model",
-                       {"N":n_agents, "width":grid_width, "height":grid_height})
-server.port = 8521 # The default
+                       {"N": n_agents, "width": grid_width, "height": grid_height})
+server.port = 8521  # The default
 server.launch()
