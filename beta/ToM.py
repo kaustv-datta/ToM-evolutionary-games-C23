ef8bc1fd1e0d2a32d92834eb62d5c880e091e500
FrancescaPerin <fanfi-@hotmail.it>
2019-11-03 03:53:32 +0100

    Added comments to ToM file and removed debugging lines

--- beta/ToM.py
+++ beta/ToM.py
@@ -4,12 +4,33 @@ import math
 PENALTY = -4
 
 class Beliefs0:
-
+	"""Zero order beliefs"""
 	def __init__(self, n_deltas, n_contexts):
+		"""Constructor for zero order beliefs
+
+		Arguments:
+			Beliefs {Beliefs0 object} -- constructed object
+			n_deltas {integer} -- number of bins for deltas interval
+			n_contexts {integer} -- number of bins for contexts interval
+		"""
 
 		self.b = np.ones([n_contexts, n_deltas])
 
 	def __call__(self, context, delta=None):
+		"""Get probability of a(ll) delta(s) bin(s) given a context bin
+
+		Arguments:
+			self {Beliefs0 object} -- beliefs on which method is called
+			context {integer} -- index of context bin
+			delta {integer} -- optional, index of delta's bin
+
+		Returns:
+			__call__(self, context, delta):
+			double -- the probability of the given delta, for the given context
+
+			__call__(self, context):
+			numpy.array of doubles -- the probability of each possible delta bin for the given context
+		"""
 
 		p = self.b[context]
 		s = np.sum(p)
@@ -21,28 +42,65 @@ class Beliefs0:
 
 
 	def observe(self, context, delta):
+		"""Update beliefs
+
+		Arguments:
+			self {Beliefs0 object} -- beliefs on which method is called
+			context {integer} -- index of observed context bin
+			delta {integer} -- index of observed delta bin
+		"""
+
 		self.b[context, delta] += 1
 
 
-def V(OSeller, OBuyer, i=-1):
+	def V(OSeller, OBuyer, i=-1):
+		"""Method to compute the discrete value function for a set of possible future offers
 
-	v = np.squeeze(i*OBuyer*np.ones([np.size(OSeller), np.size(OBuyer)]))
-	v[np.squeeze(np.transpose(OSeller[None]) > OBuyer)] = PENALTY
+		Arguments:
+			OSeller {integer or numpy.array of integers} -- single seller offer or all possible offers, taken into account
+			OBuyer {integer or numpy.array of integers} -- single buyer offer or all possible offers, taken into account
+			i {-1 or 1} -- point of view (if -1 value is computed for buyer, if 1 for seller)
 
-	if i<0:
-		return np.transpose(v)
+		Returns:
+			numpy.array of doubles -- value function of cartesian product of all given offers (of seller and buyer),
+			each row is a possible action of the acting agent, each column a possible action of the opponent
+		"""
 
-	return v
+		v = np.squeeze(i*OBuyer*np.ones([np.size(OSeller), np.size(OBuyer)]))
+		v[np.squeeze(np.transpose(OSeller[None]) > OBuyer)] = PENALTY
 
+		if i<0:
+			return np.transpose(v)
 
-def U(v, p):
-	return v.dot(p)
+		return v
+
+	def U(v, p):
+		"""Method computing the expected value of offers
+
+		Arguments:
+			v {numpy.array} -- matrix or vector representing value function for agent
+			p {numpy.array} -- vector representing probability distribution of future actions of the opponent
+
+		Returns:
+			numpy.array -- vector of expected values for each possible action of the agent
+		"""
+
+		return v.dot(p)
 
 
 class ToMAgent:
-	# General Methods and Fields for ToM agents
+	"""Super class containing general methods for any order of theory of mind"""
 
 	def __init__(self, n_deltas, n_contexts, outer, context = lambda offerSeller, offerBuyer : offerSeller - offerBuyer):
+		"""General constructor for any order of theory of mind
+
+		Arguments:
+			self {ToMAgent object} -- constructed object
+			n_deltas {integer} -- number of bins for deltas interval
+			n_contexts {integer} -- number of bins for contexts interval
+			outer {Agent.Agent} -- agent containing ToMAgent object
+			context {lambda (double, double) -> double} -- function defining how context is computed from the offer of the seller and the buyer
+		"""
 
 		self.outer = outer
 		self.direction = None
@@ -53,19 +111,55 @@ class ToMAgent:
 		self.n_contexts = n_contexts
 
 	def __call__(self, offerSeller, offerBuyer, context):
-		# Get optimal action to do
+		"""Method to get the agent's action (agent performs a decision)
+
+		Arguments:
+			self {ToMAgent} -- agent performing the action
+			offerSeller {dobule} -- offer made by seller in previous round
+			offerBuyer {dobule} -- offer made by buyer in previous round
+			context {integer} -- index of current context's bin
+
+		Returns:
+			double -- offer made
+			integer -- index of delta bin used
+		"""
+		pass
+
+	def learn(self, deltaSeller, deltaBuyer, context):
+		"""Method to updated agent's beliefs
+
+		Arguments:
+			self {ToMAgent} -- agent whose beliefs are being updated
+			deltaSeller {integer} -- index of bin of delta used by the seller
+			deltaBuyer {integer} -- integer of bin of delta used by the buyer
+			context {integer} -- integer of bin of current context
+		"""
 		pass
 
 	def setDirection(self, newDirection, opponent=None):
+		"""Setter for direction field
+
+		Arguments:
+			self {ToMAgent} -- agent whose direction is being set
+			newDirection {-1 or 1} -- new value of direction field
+			opponent {ToMAgent} -- opponent (other end of direction)
+		"""
+
 		self.direction = newDirection
 
 	def play(self, opponent):
-		# Play full bargaining with opponent
-		# print("self:", self.direction, "other: ", opponent.direction)
+		"""Method to let an agnet play with its opponent a full round of offers
+
+		Arguments:
+			self -- first agent trading
+			opponent -- other agent trading
+
+		Returns:
+			double -- concluded price at the end of the trading (None if the trading was not successfull)
+		"""
+
 		self.setDirection(1 if self.outer.owner > 0 else -1, opponent) # when it has a nest aka is a seller
 		opponent.setDirection( -1*self.direction, self)
-
-		#print("self:", self.direction, "other: ", opponent.direction)
 
 		if self.direction < 0:
 			return opponent.play(self)
@@ -73,8 +167,6 @@ class ToMAgent:
 		# The seller handles the trade (if I get here I am the seller)
 		my_offer = 1.0
 		opponent_offer = 0.0
-		#print("opponent offer:", opponent_offer)
-		#print("my offer:", my_offer)
 
 		while(my_offer > opponent_offer):
 
@@ -97,29 +189,42 @@ class ToMAgent:
 			opponent_offer = opponent_new
 			my_offer = my_new
 
-			#print("opponent offer:", opponent_offer, " action: ", deltaOpponent)
-			#print("my offer:", my_offer, " action: ", deltaMe)
-		#print("/////////////////////////////////////////////////////////")
-
 		# Add stop condition if offer of buyer is lower then the minimum price of the seller.
 		# If this is the case there is no exchange
 		if opponent_offer*opponent.outer.wealth <= self.outer.owner:
-			#print("\tNOT CONCLUDED")
 			return None
-
-		#print("\t CONCLUDED!!!!")
 
 		return opponent_offer
 
 class ToM0(ToMAgent):
-	# 0-order ToM agent
 
 	def __init__(self, n_deltas, n_contexts, outer, context = lambda offerSeller, offerBuyer : offerSeller - offerBuyer):
+		"""General constructor for any order of theory of mind
+
+		Arguments:
+			self {ToM0 object} -- constructed object
+			n_deltas {integer} -- number of bins for deltas interval
+			n_contexts {integer} -- number of bins for contexts interval
+			outer {Agent.Agent} -- agent containing ToMAgent object
+			context {lambda (double, double) -> double} -- function defining how context is computed from the offer of the seller and the buyer
+		"""
 
 		super(ToM0, self).__init__(n_deltas, n_contexts, outer, context)
 		self.beliefs = Beliefs0(n_deltas, n_contexts)
 
 	def __call__(self, offerSeller, offerBuyer, context):
+		"""Method to get the agent's action (agent performs a decision)
+
+		Arguments:
+			self {ToMAgent} -- agent performing the action
+			offerSeller {dobule} -- offer made by seller in previous round
+			offerBuyer {dobule} -- offer made by buyer in previous round
+			context {integer} -- index of current context's bin
+
+		Returns:
+			double -- offer made
+			integer -- index of delta bin used
+		"""
 
 		# Compute p dist over opponent's actions
 		p = self.beliefs(context)
@@ -134,6 +239,14 @@ class ToM0(ToMAgent):
 		return (offerSeller if self.direction>0 else offerBuyer) -self.direction*self.possibleDeltas[action], action
 
 	def learn(self, deltaSeller, deltaBuyer, context):
+		"""Method to updated agent's beliefs
+
+		Arguments:
+			self {ToMAgent} -- agent whose beliefs are being updated
+			deltaSeller {integer} -- index of bin of delta used by the seller
+			deltaBuyer {integer} -- integer of bin of delta used by the buyer
+			context {integer} -- integer of bin of current context
+		"""
 
 		if self.direction > 0:
 			self.beliefs.observe(context, deltaBuyer)
@@ -142,19 +255,48 @@ class ToM0(ToMAgent):
 
 
 class ToM1(ToMAgent):
-	# 1-order ToM agent
 
 	def __init__(self, n_deltas, n_contexts, outer, context = lambda offerSeller, offerBuyer : offerSeller - offerBuyer):
+		"""General constructor for any order of theory of mind
+
+		Arguments:
+			self {ToM1 object} -- constructed object
+			n_deltas {integer} -- number of bins for deltas interval
+			n_contexts {integer} -- number of bins for contexts interval
+			outer {Agent.Agent} -- agent containing ToMAgent object
+			context {lambda (double, double) -> double} -- function defining how context is computed from the offer of the seller and the buyer
+		"""
 
 		super(ToM1,self).__init__(n_deltas, n_contexts, outer, context)
 		self.model = ToM0(n_deltas, n_contexts, None, None)
 
 	def setDirection(self, newDirection, opponent):
+		"""Setter for direction field
+
+		Arguments:
+			self {ToMAgent} -- agent whose direction is being set
+			newDirection {-1 or 1} -- new value of direction field
+			opponent {ToMAgent} -- opponent (other end of direction)
+			"""
+
 		super(ToM1, self).setDirection(newDirection, opponent)
+
 		self.model.outer = opponent
 		self.model.setDirection(-1*self.direction)
 
 	def __call__(self, offerSeller, offerBuyer, context):
+		"""Method to get the agent's action (agent performs a decision)
+
+		Arguments:
+			self {ToMAgent} -- agent performing the action
+			offerSeller {dobule} -- offer made by seller in previous round
+			offerBuyer {dobule} -- offer made by buyer in previous round
+			context {integer} -- index of current context's bin
+
+		Returns:
+			double -- offer made
+			integer -- index of delta bin used
+		"""
 
 		# Predict action of opponent
 		opponent_action, _ = self.model(offerSeller, offerBuyer, context)
@@ -176,6 +318,14 @@ class ToM1(ToMAgent):
 		return (offerSeller if self.direction>0 else offerBuyer)-self.direction*self.possibleDeltas[action], action
 
 	def learn(self, deltaSeller, deltaBuyer, context):
+		"""Method to updated agent's beliefs
+
+		Arguments:
+			self {ToMAgent} -- agent whose beliefs are being updated
+			deltaSeller {integer} -- index of bin of delta used by the seller
+			deltaBuyer {integer} -- integer of bin of delta used by the buyer
+			context {integer} -- integer of bin of current context
+		"""
 
 		self.model.learn(deltaSeller, deltaBuyer, context)
 
